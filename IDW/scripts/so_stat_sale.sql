234bfb40526b8c80de20e18a8dac5072f6d9736c
SOUVIK BISWAS <souvbisw@in.ibm.com>
2019-09-13 15:44:05 +0530

    adding a new line in line#3 by sobis3

--- IDW/scripts/so_stat_sale.sql
+++ IDW/scripts/so_stat_sale.sql
@@ -1,8 +1,4 @@
 # Sample file format for so_stat_sale.sql
-<<<<<<< HEAD
 # Adding a new line in so_stat_sale.sql job
 # Adding a new line at line no 3 in so_stat_sale job by sobis3
-=======
-# Adding a new line in so_stat_sale.sql job 
-# Adding a second line in so_stat_sale.sql job
->>>>>>> c44f0a555a6f33892343be10846e136d08412685
+
