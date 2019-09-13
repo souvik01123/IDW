7f11c6b3cb49b42934896ed9d81f0aff238beec0
SOUVIK BISWAS <souvbisw@in.ibm.com>
2019-09-13 16:45:35 +0530

    adding line 4

--- IDW/scripts/so_stat_sale.sql
+++ IDW/scripts/so_stat_sale.sql
@@ -1,4 +1,5 @@
 # Sample file format for so_stat_sale.sql
 # Adding a new line in so_stat_sale.sql job
 # Adding a new line at line no 3 in so_stat_sale job by sobis3
+# Adding a new line at line no 4 in so_stat_sale job by sobis3
 
