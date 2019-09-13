0c4e3e75a8181ea8eca7468201e499cce4599080
SOUVIK BISWAS <souvbisw@in.ibm.com>
2019-09-13 17:15:04 +0530

    adding 5th line by sobis3

--- IDW/scripts/so_stat_sale.sql
+++ IDW/scripts/so_stat_sale.sql
@@ -2,4 +2,4 @@
 # Adding a new line in so_stat_sale.sql job 
 # Adding a second line in so_stat_sale.sql job
 # Adding a third line in so_stat_sale.sql job
-
+# Adding a fourth line in so_stat_sale.sql job
