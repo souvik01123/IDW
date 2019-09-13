7a402804ddae9370a66e0a8dc5a337c52ec55627
gayathiri <sugagayathiri@gmail.com>
2019-09-13 17:09:40 +0530

    commit 1

--- IDW/scripts/so_stat_sale.sql
+++ IDW/scripts/so_stat_sale.sql
@@ -1,11 +1,4 @@
 # Sample file format for so_stat_sale.sql
-<<<<<<< HEAD
 # Adding a new line in so_stat_sale.sql job 
 # Adding a second line in so_stat_sale.sql job
-# Adding a Tird line in so_stat_sale.sql job
-=======
-# Adding a new line in so_stat_sale.sql job
-# Adding a new line at line no 3 in so_stat_sale job by sobis3
-# Adding a new line at line no 4 in so_stat_sale job by sobis3
 
->>>>>>> 234bfb40526b8c80de20e18a8dac5072f6d9736c
