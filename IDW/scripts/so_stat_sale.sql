191e4a7c722ec925fe11928f2c24e3863ebe164a
SOUVIK BISWAS <souvbisw@in.ibm.com>
2019-09-13 13:22:11 +0530

    adding 2nd change in 3rd line of so_stat_sale.sql by sobis3

--- IDW/scripts/so_stat_sale.sql
+++ IDW/scripts/so_stat_sale.sql
@@ -1,2 +1,3 @@
 # Sample file format for so_stat_sale.sql
-# Adding a new line in so_stat_sale.sql job 
+# Adding a new line in so_stat_sale.sql job
+# Adding a new line at line no 3 in so_stat_sale job by sobis3
